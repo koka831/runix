ba6cba994fd426ec80c17279266cdc80bf8d251c
koka <koka.code@gmail.com>
2018-01-24 02:27:01 +0900

    style: use lower case

--- src/arch/x86_64/ipl.asm
+++ src/arch/x86_64/ipl.asm
@@ -4,13 +4,13 @@ section .text
 bits 32
 
 start:
-    MOV DWORD [0xb8000], 0x2f4b2f4f
+    mov dword [0xb8000], 0x2f4b2f4f
 
 error:
-    MOV DWORD [0xb8000], 0x4f524f45
-    MOV DWORD [0xb8004], 0x4f3a4f52
-    MOV DWORD [0xb8008], 0x4f204f20
-    MOV BYTE  [0xb800a], al
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dowrd [0xb8008], 0x4f204f20
+    mov byte  [0xb800a], al
 fin:
     hlt
     jmp fin

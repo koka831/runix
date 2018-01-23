9d9cd0f800b73ac4584345ccc3f8ae65f82d6838
koka <koka.code@gmail.com>
2018-01-24 01:57:55 +0900

    feat: check if multibootable

--- src/arch/x86_64/ipl.asm
+++ src/arch/x86_64/ipl.asm
@@ -4,7 +4,27 @@ section .text
 bits 32
 
 start:
+    MOV esp, stack_top
+    ; print 'OK' to screen
     MOV DWORD [0xb8000], 0x2f4b2f4f
+chekck_boot:
+    cmp EAX, 0x36d76289
+    JNE .no_multiboot
+    RET
+.no_multiboot:
+    MOV al, "0"
+    jmp error
+
+error:
+    MOV DWORD [0xb8000], 0x4f524f45
+    MOV DWORD [0xb8004], 0x4f3a4f52
+    MOV DWORD [0xb8008], 0x4f204f20
+    MOV BYTE  [0xb800a], al
 fin:
     hlt
     jmp fin
+
+section .bss
+stack_bottom:
+    resb    64
+stack_top:

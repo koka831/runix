66fb65430101d02a2b3bd5e95a1c227f9f61aeb3
koka <koka.code@gmail.com>
2018-01-25 22:27:11 +0900

    feat: print hello world yay

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![feature(lang_items)]
+#![no_std]
+
+extern crate rlibc;
+#[no_mangle]
+pub extern fn boot() {
+    let hello = b"Hello, World";
+    let color = 0x1f; // f:white b:blue
+    let mut hello_clr = [color; 24];
+    for (i, chr) in hello.into_iter().enumerate() {
+        hello_clr[i * 2] = *chr;
     }
+
+    let buf_ptr = (0xb8000 + 1988) as *mut _;
+    unsafe { *buf_ptr = hello_clr };
+
+    loop {}
 }
+
+#[lang = "eh_personality"] #[no_mangle] pub extern fn eh_personality() {}
+#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! { loop{} }
